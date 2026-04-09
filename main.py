77c5496dfc00171c22c96bc167aa42d2c3456036
vb1308 <vishnubargav06@gmail.com>
2026-04-09 09:16:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    # render_template automatically looks in the /templates folder
     return render_template('index.html', title="Home Page")
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8090, debug=True)
+    # Local development server
+    app.run(host='127.0.0.1', port=8080, debug=True)
