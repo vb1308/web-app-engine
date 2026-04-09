ae899c17f640c0f3ac2581d68d01c9c7f1a0cfe6
vb1308 <vishnubargav06@gmail.com>
2026-04-09 09:05:01 +0530

    Change server port from 8080 to 8090

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ def home():
     return render_template('index.html', title="Home Page")
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=8090, debug=True)
